56960e19e02847f261ef788b9ef9b462fc3c9a61
Gourav0902 <singhgourav0902@gmail.com>
2024-12-06 19:11:20 -0500

    Add the Gui features

--- Server/ServerGS.py
+++ Server/ServerGS.py
@@ -1,11 +1,12 @@
 import socket
 import os, time
 import json
+import PySimpleGUI as sg
 
 s = socket.socket()
 host = ''
 port = 5000
-s.blind((host, port))
+s.bind((host, port))
 s.listen(5)
 
 #gets the Core Temperature from Pi, ref https://github.com/nicmcd/vcgencmd/blob/master/README.md
@@ -25,15 +26,33 @@ def get_gpu_memory():
     Gm = os.popen('vcgencmd get_mem gpu').readline()
     return round(float(Gm.replace("gpu=", "").replace("M", "")), 1)
 
+layout = [
+    [sg.Text(f"Temperature:{get_temp()}\u2103", size=(30, 1))],
+    [sg.Text(f"Volt:{get_volt()}V ", size=(30, 1))],
+    [sg.Text(f"Arm Memory:{get_Arm_memory()}MB", size=(30, 1))],
+    [sg.Text(f"Frequency:{get_Clock_frequency()}\u3392", size=(30, 1))],
+    [sg.Text(f"GPU Memory:{get_gpu_memory()}MB", size=(30, 1))],
+   
+    [sg.Button('Exit')]
+]
+
+window = sg.Window('Server Monitoring', layout)
+
 
 
 try:
     while True:
-      c, addr = s.accept()
-      print ('Got connection from',addr)
-      res = bytes(str(f_dict), 'utf-8') # needs to be a byte
-      c.send(res) # sends data as a byte type
+        event, values = window.read()
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            window.close()
+            break
+        
+        c, addr = s.accept()
+        print ('Got connection from',addr)
+        res = bytes(str(f_dict), 'utf-8') # needs to be a byte
+        c.send(res) # sends data as a byte type
 except KeyboardInterrupt:
     print ("Good Bye")
     exit()
+
 
