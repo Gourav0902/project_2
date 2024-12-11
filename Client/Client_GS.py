dcd7b0c33fc5bb2bec00fe5396a6cf76ec5c6aab
Gourav0902 <singhgourav0902@gmail.com>
2024-12-10 19:58:35 -0500

    change the code style according to the given example on the DC connect and add Gui

--- Client/Client_GS.py
+++ Client/Client_GS.py
@@ -1,13 +1,8 @@
-# This server runs on Pi, sends Pi's your 4 arguments from the vcgencmds, sent as Json object.
-
-# details of the Pi's vcgencmds - https://www.tomshardware.com/how-to/raspberry-pi-benchmark-vcgencmd
-# more vcgens on Pi 4, https://forums.raspberrypi.com/viewtopic.php?t=245733
-# more of these at https://www.nicm.dev/vcgencmd/
-
 import socket
-import os, time
-import json
-import PySimpleGUI as sg
+import sys
+import json, time
+import os, io
+
 def Hardware_check():
     try:
         with open("/proc/cpuinfo", "r") as cpuinfo:
@@ -16,81 +11,67 @@ def Hardware_check():
                     return True
     except FileNotFoundError:
         pass
-    return false
+    return False
+
 if not Hardware_check():
-    sg.pop("Check Device")
+    sg.popup("Check Device")
     exit(0)
 
-s = socket.socket()
-host = '' # Localhost
-port = 5000
-s.bind((host, port))
-s.listen(5)
-s.setblocking(False)
-s.settimeout(1.0)
 
-#gets the Core Temperature from Pi, ref https://github.com/nicmcd/vcgencmd/blob/master/README.md
+sock = socket. socket()
+    
+
+port = 5000
+#address = "192.168.10.121" # of server, so we can read the vcgencmd data
+address = "10.102.13.191"# to run on Pi with local server
+
+# Functions to get various Pi parameters using vcgencmd
 def get_temp():
-    t = os.popen('vcgencmd measure_temp').readline() #gets from the os, using vcgencmd - the core-temperature
+    t = os.popen('vcgencmd measure_temp').readline()
     return round(float(t.replace("temp=", "").replace("'C", "")), 1)
+
 def get_volt():
     v = os.popen('vcgencmd measure_volts core').readline()
     return round(float(v.replace("volt=", "").replace("V", "")), 1)
+
 def get_Arm_memory():
     m = os.popen('vcgencmd get_mem arm').readline()
     return round(float(m.replace("arm=", "").replace("M", "")), 1)
+
 def get_Clock_frequency():
     c = os.popen('vcgencmd measure_clock arm').readline()
     return round(int(c.split('=')[1]) / 1_000_000, 1)
+
 def get_gpu_memory():
     Gm = os.popen('vcgencmd get_mem gpu').readline()
     return round(float(Gm.replace("gpu=", "").replace("M", "")), 1)
-# initialising json object string
-
-ini_string = {"Temperature":get_temp(),
-              "Voltage": get_volt(),
-              "Arm_Memory":get_Arm_memory(),
-              "Arm_Clock":get_Clock_frequency() ,
-              "GPU_Memory": get_gpu_memory()
-              }
-
-f_dict = json.dumps(ini_string) # The eval() function evaluates JavaScript code represented as a string and returns its completion value.
-sg. theme ('Light Brown 4')
-CIRCLE = '⚫' # unicode symbol 9899
-CIRCLE_OUTLINE = '⚪' # unicode symbol 9890
-
-def LED(color, key) :
-  
-    return sg.Text(CIRCLE_OUTLINE, text_color=color, key=key)
-
-layout = [
-    [sg.Text('Status '), sg.Text('Disconnected', key="-STATUS-")],  
-    [sg.Button("Exit")],
-    [sg.Text('', size=(30, 1), key='-Message-')],
-]
-
-window = sg.Window("Client Monitoring",layout)
-
 try:
-    while True:
-        try:
-            c, addr = s.accept()
-            print(f'Got connection from {addr}')
-            window['-STATUS-'].update("Connected")
-            res = bytes(str(f_dict), 'utf-8')  # Convert JSON string to byte format
-            c.send(res)  # Send the data
-            c.close()  # Properly close the connection after sending data
-        except socket.timeout:
-            # If no connection, just continue and allow the GUI to update
-            pass
+    sock.connect((address, port))
+    for i in range(10):
+        jsonResult = {
+                        "Temperature": get_temp(),
+                        "Voltage": get_volt(),
+                        "Arm_Memory": get_Arm_memory(),
+                        "Arm_Clock": get_Clock_frequency(),
+                        "GPU_Memory": get_gpu_memory()
+                    }
 
-        # Read events from the GUI
-        event, values = window.read(timeout=1000)  # Timeout of 100ms to allow non-blocking GUI interaction
+        #jsonResult = {"thing": [{"temp":"You're"}], "volts":v, "temp-core":core, "it =": i}
 
-        if event == sg.WIN_CLOSED or event == 'Exit':
-            break
-except KeyboardInterrupt:
-    print ("Good Bye")
+        jsonResult = json.dumps(jsonResult)
+        jsonbyte = bytearray(jsonResult,"UTF-8")
+        print("this Json byte, sent ->", jsonbyte)
+        
+
+        #print(v, " it = ",i, " ",core)
+        #sock. send(jsonResult)
+        sock.send(jsonbyte)
+        time.sleep(2)
+
+except socket.gaierror: # = short form for getaddrinfo()
+
+    print('There an error resolving the host')
+    sock.close()
 finally:
-    window.close()  # Ensure that the window is properly closed
+    print("Sorry lost connection with server")
     exit()
--- Server/ServerGS.py
+++ Server/ServerGS.py
@@ -1,58 +1,68 @@
+'''This is the server, should run first, in sperarte thonny instance,
+gets client (pi) data'''
+
 import socket
-import os, time
-import json
+import json, time
 import PySimpleGUI as sg
 
-s = socket.socket()
-host = ''
+sock = socket.socket()
+print("Socket created ... ")
+
 port = 5000
-s.bind((host, port))
-s.listen(5)
+#sock.bind(('', port))ret1 = data["Temperature"] # Temperature value
+            
 
-#gets the Core Temperature from Pi, ref https://github.com/nicmcd/vcgencmd/blob/master/README.md
-def get_temp():
-    t = os.popen('vcgencmd measure_temp').readline() #gets from the os, using vcgencmd - the core-temperature
-    return round(float(t.replace("temp=", "").replace("'C", "")), 1)
-def get_volt():
-    v = os.popen('vcgencmd measure_volts core').readline()
-    return round(float(v.replace("volt=", "").replace("V", "")), 1)
-def get_Arm_memory():
-    m = os.popen('vcgencmd get_mem arm').readline()
-    return round(float(m.replace("arm=", "").replace("M", "")), 1)
-def get_Clock_frequency():
-    c = os.popen('vcgencmd measure_clock arm').readline()
-    return round(int(c.split('=')[1]) / 1_000_000, 1)
-def get_gpu_memory():
-    Gm = os.popen('vcgencmd get_mem gpu').readline()
-    return round(float(Gm.replace("gpu=", "").replace("M", "")), 1)
 
+sock.bind(('10.102.13.191', port)) # to run on Pi with local client
+sock.listen(5)
+
+
+c, addr = sock.accept()
+sg. theme ('Light Brown 4')
 layout = [
-    [sg.Text(f"Temperature:{get_temp()}\u2103", size=(30, 1))],
-    [sg.Text(f"Volt:{get_volt()}V ", size=(30, 1))],
-    [sg.Text(f"Arm Memory:{get_Arm_memory()}MB", size=(30, 1))],
-    [sg.Text(f"Frequency:{get_Clock_frequency()}\u3392", size=(30, 1))],
-    [sg.Text(f"GPU Memory:{get_gpu_memory()}MB", size=(30, 1))],
-   
-    [sg.Button('Exit')]
+    [sg.Text('Temperature:', size=(15, 1)), sg.Text('', size=(15, 1), key='-TEMP-')],
+    [sg.Text('Voltage:', size=(15, 1)), sg.Text('', size=(15, 1), key='-VOLT-')],
+    [sg.Text('Arm Memory:', size=(15, 1)), sg.Text('', size=(15, 1), key='-ARM_MEMORY-')],
+    [sg.Text('Arm Clock:', size=(15, 1)), sg.Text('', size=(15, 1), key='-ARM_CLOCK-')],
+    [sg.Text('GPU Memory:', size=(15, 1)), sg.Text('', size=(15, 1), key='-GPU_MEMORY-')],
+    [sg.Button("Exit")],
 ]
-
-window = sg.Window('Server Monitoring', layout)
-
-
-
-try:
+window = sg.Window("Server Monitoring", layout)
+def main():
     while True:
-        event, values = window.read()
-        if event in (sg.WINDOW_CLOSED, "Exit"):
-            window.close()
+        event, values = window.read(timeout=100)  # Timeout of 100ms to allow non-blocking GUI interaction
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
             break
-        
-        c, addr = s.accept()
-        print ('Got connection from',addr)
-        res = bytes(str(f_dict), 'utf-8') # needs to be a byte
-        c.send(res) # sends data as a byte type
-except KeyboardInterrupt:
-    print ("Good Bye")
-    exit()
+        try:
+            jsonReceived = (c.recv(1024).decode())
+            
+            if jsonReceived == '':
+                print("connection lost")
+                exit()
+            data = json.loads(jsonReceived) #creates the Json string
+            ret1 = data["Temperature"] # Temperature value
+            ret2 = data["Voltage"] # Voltage value
+            ret3 = data["Arm_Memory"]# ARM memory usage
+            ret4 = data["Arm_Clock"]# ARM clock speed
+            ret5 = data["GPU_Memory"]# GPU memory usage
+
+            window['-TEMP-'].update(f"{ret1}\u2103")
+            window['-VOLT-'].update(f"{ret2} V")
+            window['-ARM_MEMORY-'].update(f"{ret3} MB")
+            window['-ARM_CLOCK-'].update(f"{ret4} MHz")
+            window['-GPU_MEMORY-'].update(f"{ret5} MB")
+        except (socket.error, json.JSONDecodeError):
+            print("Check server connection")
+            break
+    window.close()
+    c.close
 
 
+
+if _name== 'main_':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Bye .... ")
+        exit()
