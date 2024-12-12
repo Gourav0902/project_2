015d75266f16c0488dc425bd9f4bac867aa0c490
Gourav0902 <singhgourav0902@gmail.com>
2024-12-12 12:23:10 -0500

    add threading and led gui

--- Client/Client_GS.py
+++ Client/Client_GS.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import json, time
 import os, io
+import threading
 import PySimpleGUI as sg
 
 def Hardware_check():
@@ -20,11 +21,11 @@ if not Hardware_check():
 
 
 sock = socket. socket()
-    
+   
 
 port = 5000
 #address = "192.168.10.121" # of server, so we can read the vcgencmd data
-address = "10.102.13.191"# to run on Pi with local server
+address = "10.102.13.149"# to run on Pi with local server
 
 # Functions to get various Pi parameters using vcgencmd
 def get_temp():
@@ -46,31 +47,38 @@ def get_Clock_frequency():
 def get_gpu_memory():
     Gm = os.popen('vcgencmd get_mem gpu').readline()
     return round(float(Gm.replace("gpu=", "").replace("M", "")), 1)
+def get_throttling():
+    throttling = os.popen('vcgencmd get_throttled').readline()
+    if throttling == '0x0':
+        return "NO Throttling"
+    else:
+        return "Throttling Detected"
 sg.theme('Light Brown 4')
-
+CIRCLE = '\u26AB'
+CIRCLE_OUTLINE = '⚪'
+def LED(color, key) :
+    return sg.Text(CIRCLE_OUTLINE, text_color=color, key=key)
 layout = [
-    [sg.Text('Status '), sg.Text('Disconnected', key="-STATUS-")],  
+    [sg.Text('Status '),LED('Green', '-LED0-'), sg.Text('Disconnected', key="-STATUS-")],  
     [sg.Button("Exit")],
     
 ]
 
-window = sg.Window("Client Monitoring", layout)
-while True:
-    try:
-        event, values = window.read(timeout=100)  # Timeout of 100ms to allow non-blocking GUI interaction
 
-        if event == sg.WIN_CLOSED or event == 'Exit':
-            break
+window = sg.Window("Client Monitoring", layout, finalize = True)
+def send_data():
         try:
             sock.connect((address, port))
-           
+            window['-STATUS-'].update("Connected")
+            window['-LED0-'].update(CIRCLE)
             for i in range(50):
                 jsonResult = {
                                 "Temperature": get_temp(),
                                 "Voltage": get_volt(),
                                 "Arm_Memory": get_Arm_memory(),
                                 "Arm_Clock": get_Clock_frequency(),
-                                "GPU_Memory": get_gpu_memory()
+                                "GPU_Memory": get_gpu_memory(),
+                                "Throttling": get_throttling()
                             }
 
                 #jsonResult = {"thing": [{"temp":"You're"}], "volts":v, "temp-core":core, "it =": i}
@@ -82,18 +90,35 @@ while True:
 
                 #print(v, " it = ",i, " ",core)
                 #sock. send(jsonResult)
+                
                 sock.send(jsonbyte)
-                window['-STATUS-'].update("Connected")
+                
                 time.sleep(2)
 
         except socket.gaierror: # = short form for getaddrinfo()
 
-            print('There an error resolving the host')
-            sock.close()
+            sg.popup('There an error resolving the host')
+            window['-STATUS-'].update("Disconnected")
             
-    except KeyboardInterrupt:
-        print("Good Bye")
-        break
-    finally:
-        print("Good Bye")
-        exit()
+        
+        finally:
+            sock.close()
+
+def main():
+    thread = threading.Thread(target=send_data, daemon=True)
+    thread.start()
+    while True:
+        try:
+            event, values = window.read(timeout=100)  # Timeout of 100ms to allow non-blocking GUI interaction
+
+            if event == sg.WIN_CLOSED or event == 'Exit':
+                break           
+        except KeyboardInterrupt:
+            sg.popup("Good Bye")
+            break
+        
+    sock.close()
+    window.close()
+
+if _name_ == "_main_":
+    main()
